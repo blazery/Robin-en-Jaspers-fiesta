a2438c0ab88eb5bbe65d5623af7c38e4c870ad95
blazery <jaspe47@hotmail.com>
2016-10-22 01:04:18 +0200

    file reader fix, import still strange

--- src/FileReader.py
+++ src/FileReader.py
@@ -8,7 +8,7 @@ class FileReader:
 
     def readFile(self, path):
         try:
-            file = open("path", 'r')
+            file = open(path, 'r')
         except:
             print("LOADING FILE ERROR")
             print("File could not be found")
@@ -16,15 +16,19 @@ class FileReader:
 
         properties_dictionary = dict()
         string_list = file.readlines()
-        print(string_list)
+
         for s in string_list:
             temp_key = s.split(':')[0].strip(' ').upper()
             temp_value = None
+
             if ':' in s:
                 temp_value = s.split(':')[1].strip(' ').strip('\n')
+
             if temp_key in self.properties.properties:
                 properties_dictionary[temp_key] = temp_value
+
         self.last_read_file = properties_dictionary
+        return  properties_dictionary
 
 test = FileReader()
-test.readFile("")
+print(test.readFile("emptyfile.txt"))
