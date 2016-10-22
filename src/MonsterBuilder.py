ec3d1878f43c3a3150069657ff028cc6633d251b
robin.kneepkens@hotmail.com <robin.kneepkens@hotmail.com>
2016-10-22 15:15:03 +0200

    Made it so open slots for body parts can be counted

--- src/MonsterBuilder.py
+++ src/MonsterBuilder.py
@@ -5,18 +5,34 @@ import GameData
 class MonsterBuilder:
     template = None
     attach_hierarchy = {}
+    slots = {}
 
     def __init__(self, attributes):
         self.template = attributes
-        self.attach_hierarchy = GameData.GameData().loadHierarchy("MonsterHierarchy.txt")
+        self.attach_hierarchy = GameData.GameData().monster_hierarchy
+    
+    def calcSpaceForBP(self):
+        for hierarchy_part in self.attach_hierarchy:
+            for template_part in self.template.keys():
+                if template_part in self.attach_hierarchy[hierarchy_part]:
+                    if hierarchy_part != "TORSO":
+                        if hierarchy_part in self.template.keys():
+                            if template_part in self.slots.keys():
+                                self.slots[template_part] += self.template[hierarchy_part]
+                            else:
+                                self.slots[template_part] = self.template[hierarchy_part]
+                        else:
+                            if not template_part in self.slots.keys():
+                                self.slots[template_part] = 0
 
     def buildMonster(self):
         proto_monster = Monster.Monster()
         for entry in self.template:
             if entry in self.attach_hierarchy["TORSO"]:
                 for i in range(self.template[entry]):
-                    print("Entry: ", entry)
                     proto_monster.torso.contains[entry + str(i)] = Bodypart.Bodypart(entry)
-           # elif entry in self.attach_hierarchy["ARMS"]:
-           #     proto_monster.torso["ARMS"][entry] = self.template[entry]
+            for torso_part in self.attach_hierarchy["TORSO"]:
+                for i in range(self.template[entry]):
+                    if entry in self.attach_hierarchy[torso_part] and torso_part in self.template.keys():
+                        proto_monster.torso.contains
         return proto_monster
