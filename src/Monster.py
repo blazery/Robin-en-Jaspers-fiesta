a01023486e598058eb545f48ea5b89bc55a5ffa6
blazery <jaspe47@hotmail.com>
2016-10-22 16:18:29 +0200

    monster methods

--- src/Monster.py
+++ src/Monster.py
@@ -2,3 +2,26 @@ import Bodypart
 
 class Monster:
     torso = Bodypart.Bodypart("TORSO")
+
+    def calcTotalHP(self, par_hp = torso.hit_points, par_part = torso):
+        hp = par_hp
+
+        for part in par_part.contains:
+            hp += part.hit_points
+
+            if part.contains != {}:
+                hp += self.calcTotalHP(hp, part)
+
+        return hp
+
+    def countEyes(self, par_part = torso):
+        eyes = 0
+
+        for part in par_part.contains:
+            if part.part_type == "EYES":
+                eyes += 1
+
+            if part.contains != {}:
+                eyes += self.countEyes(part)
+
+        return eyes
